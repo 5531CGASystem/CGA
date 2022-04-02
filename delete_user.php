5166ac7ae8ccee56376d289f0d3ec3c08a23229e
bajajp1994 <bajajp1994@gmail.com>
2022-04-02 11:05:00 -0400

    Add files via upload

---
+++ delete_user.php
@@ -0,0 +1,32 @@
+<?php
+/* Database credentials. */
+define('DB_SERVER', 'rtc5531.encs.concordia.ca');
+define('DB_USERNAME', 'rtc55314');
+define('DB_PASSWORD', 'khbbmG');
+define('DB_NAME', 'rtc55314');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link == false) {
+	die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+if(!isset($_GET['id'])){
+        // redirect to show page
+        die('id not provided');
+    }
+$id = (int)$_GET['id'];
+ 
+
+   $sql = "DELETE FROM users WHERE user_id = '$id'";
+    if ($link->query($sql) === TRUE) {
+				 
+				 echo "User deleted successfully!!";
+                  } else {
+                     echo "Error deleting record: " . $link->error;
+                }
+    // Close connection
+    mysqli_close($link);
+
+?>
