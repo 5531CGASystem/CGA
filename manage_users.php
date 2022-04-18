6cf729ee4ca162ef0ea48c10adb44f70548173f6
Vikram Nisarga <vikramnisarga1@gmail.com>
2022-04-18 11:45:52 -0400

    fix manage users

--- manage_users.php
+++ manage_users.php
@@ -1,28 +1,13 @@
 <?php
 //40197292
-/* Database credentials. */
-define('DB_SERVER', 'rtc5531.encs.concordia.ca');
-define('DB_USERNAME', 'rtc55314');
-define('DB_PASSWORD', 'khbbmG');
-define('DB_NAME', 'rtc55314');
 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-// Check connection
-if($link == false) {
-	die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+include "includes/head.php";
 $result = mysqli_query($link,"SELECT * FROM users");
 
-echo "<html>
-<head>
-<title>Users</title>
-</head>
-
-<body style='background-color:#faf0e6'>
+?>
+<div class="content">
 </br>
-<h1>Users:</h1>
+<h1>Manage Users:</h1>
 </br>
 <div  class='form-group'>
    <a href='create_user.php'>
@@ -30,18 +15,18 @@ echo "<html>
 </a> 
 </div>
 </br></br>
-</body>
-</html>";
 
-echo "<table border='1'>
+
+<table border='1'>
 <tr>
 <th>Username</th>
 <th>Email</th>
 <th>First_Name</th>
 <th>Last_Name</th>
-<th></th>
-</tr>";
+<th>Options</th>
+</tr>
 
+<?php
 while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
@@ -56,3 +41,6 @@ echo "</table>";
 
 mysqli_close($link);
 ?>
+</div>
+</body>
+</html>
