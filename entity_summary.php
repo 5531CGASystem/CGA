a6f1f2519baf7cc4947fe960e7e674b7e0784a89
Vikram Nisarga <vikramnisarga1@gmail.com>
2022-04-17 22:48:02 -0400

    fixing merge entity_summary

--- entity_summary.php
+++ entity_summary.php
@@ -107,102 +107,100 @@ if (!isset($_SERVER['HTTP_REFERER'])) {
         $type = 'Other';
     }
 
-echo "Course Info: <font color='darkblue'>" . $_SESSION['code'] . " - " . $_SESSION['term'] . " " . $_SESSION['year'] . " (Section " . $_SESSION['section_name'] . ")</font><br>";
-echo "Entity name: <font color='darkblue'>" . $name . "</font><br>";
-echo "Post date: <font color='darkblue'>" . $post_date . "</font><br>";
-echo "Due date: <font color='darkblue'>" . $due_date . "</font><br>";
-echo "Type: <font color='darkblue'>" . $type . "</font><br>";
-echo "Work Type: <font color='darkblue'>" . ucfirst($work_type) . "</font><br>";
-echo "Viewable to (assigned to): <font color='darkblue'>" . $view . "</font><br>";
-echo "File attachment: <a href='download.php?name=".$real_file_name."'> $file </a><br>";
-echo "Description: <font color='darkblue'>" . $desc . "</font><br>";
-echo "<p></p>";
-echo "Files uploaded to this entity:";
-echo "<p></p>";
-echo "<table><tbody><tr><th>File Name</th><th>Author</th><th>Date Uploaded</th><th>Viewable To</th><th>Actions</th></tr>";
-// Get marked entity data
-$data = $link->query("SELECT mf.file_id, mf.file_name, mf.file_location, mf.uploaded_at, mf.category_id, u.username FROM marked_entity_files mf, users u WHERE mf.marked_entity_id=" . $_SESSION['entity_id'] . " and mf.uploaded_by = u.user_id");
-if($data -> num_rows>0){
-    echo "<tb>";
-    while($row = mysqli_fetch_array($data,MYSQLI_NUM)){
-        $file_id = $row[0];
-        $file_name = $row[1];
-        $file_location = $row[2];
-        $uploaded_at = $row[3];
-        $category_id = $row[4];
-        $uploaded_by = $row[5];
+    echo "Course Info: <font color='darkblue'>" . $_SESSION['code'] . " - " . $_SESSION['term'] . " " . $_SESSION['year'] . " (Section " . $_SESSION['section_name'] . ")</font><br>";
+    echo "Entity name: <font color='darkblue'>" . $name . "</font><br>";
+    echo "Post date: <font color='darkblue'>" . $post_date . "</font><br>";
+    echo "Due date: <font color='darkblue'>" . $due_date . "</font><br>";
+    echo "Type: <font color='darkblue'>" . $type . "</font><br>";
+    echo "Work Type: <font color='darkblue'>" . ucfirst($work_type) . "</font><br>";
+    echo "Viewable to (assigned to): <font color='darkblue'>" . $view . "</font><br>";
+    echo "File attachment: <a href='download.php?name=" . $real_file_name . "'> $file </a><br>";
+    echo "Description: <font color='darkblue'>" . $desc . "</font><br>";
+    echo "<p></p>";
+    echo "Files uploaded to this entity:";
+    echo "<p></p>";
+    echo "<table><tbody><tr><th>File Name</th><th>Author</th><th>Date Uploaded</th><th>Viewable To</th><th>Actions</th></tr>";
+    // Get marked entity data
+    $data = $link->query("SELECT mf.file_id, mf.file_name, mf.file_location, mf.uploaded_at, mf.category_id, u.username FROM marked_entity_files mf, users u WHERE mf.marked_entity_id=" . $_SESSION['entity_id'] . " and mf.uploaded_by = u.user_id");
+    if ($data->num_rows > 0) {
+        echo "<tb>";
+        while ($row = mysqli_fetch_array($data, MYSQLI_NUM)) {
+            $file_id = $row[0];
+            $file_name = $row[1];
+            $file_location = $row[2];
+            $uploaded_at = $row[3];
+            $category_id = $row[4];
+            $uploaded_by = $row[5];
 
-        $data2 = $link->query("SELECT viewable_to FROM forum_categories WHERE category_id=$category_id");
-        if($data2 -> num_rows>0){
-            $cat_data = $data2->fetch_assoc();
-            $file_viewable_to = $cat_data['viewable_to'];
-            // Converting $viewable_to to more readable text
-            if($file_viewable_to == ',all,'){
-                $file_view = "All";
-            }
-            else{
-                $file_viewable_to = substr($file_viewable_to, 1, -1);
-                $file_view_array = explode (",", $file_viewable_to);
-                $file_view = "";
-                foreach($file_view_array as $value){
-                    $data3 = $link->query("SELECT name FROM rtc55314.groups WHERE group_id=$value");
-                    if($data3 -> num_rows>0){
-                        $file_group_data = $data3->fetch_assoc();
-                        $file_group_name = $file_group_data['name'];
-                        $file_view = $file_view . $file_group_name . ", ";
-                    }
-                }
-                $file_view = substr($file_view, 0, -2);
-            }
-        }
-
-        // Display all the files uploaded to entity for admin, instructor, TA or all
-        if ($file_view == 'All' || $_SESSION['role_id'] < 4){
-            echo "<tr><td> <a href='download.php?name=". $file_location ."'> " . $file_name . "</a></td><td>" . $uploaded_by . "</td><td>" . $uploaded_at . "</td><td>" . $file_view . "</td><td>";
-            // Check if user has delete access
-            if ($_SESSION['role_id'] < 3 || $uploaded_by == $_SESSION['username']){
-                echo "<form class='form-button' method=post action='includes/delete_file_to_entity.php'>";
-                echo "<button type='submit' name='delete' value=$file_id onclick=\"return confirm('Are you sure you want to delete this file?')\">Delete</button>";
-                echo "</form>";
-            }
-            echo "</td></tr>";
-        }
-        // Display the files uploaded to entity available for student
-        else{
-            // Display the files that were available to the student when they were part of a particular group
-            foreach($file_view_array as $value){
-                $data4 = $link->query("SELECT join_group_date, left_group_date FROM group_users WHERE group_id=$value AND user_id=" . $_SESSION['id']);
-                if($data4 -> num_rows>0){
-                    $group_data = $data4->fetch_assoc();
-                    $join_group_date = $group_data['join_group_date'];
-                    $left_group_date = $group_data['left_group_date'];
-                    $join_group_date2 = strtotime($join_group_date);
-                    $left_group_date2 = strtotime($left_group_date);
-                    $uploaded_at2 = strtotime($uploaded_at);
-                    if(!($join_group_date == "") && ($left_group_date == "")){
-                        echo "<tr><td> <a href='download.php?name=". $file_location ."'> " . $file_name . "</a></td><td>" . $uploaded_by . "</td><td>" . $uploaded_at . "</td><td>" . $file_view . "</td><td>";
-                        // Allow delete for the person who uploaded the file
-                        if($uploaded_by == $_SESSION['username']){
-                            echo "<form class='form-button' method=post action='includes/delete_file_to_entity.php'>";
-                            echo "<button type='submit' name='delete' value=$file_id onclick=\"return confirm('Are you sure you want to delete this file?')\">Delete</button>";
-                            echo "</form>";
+            $data2 = $link->query("SELECT viewable_to FROM forum_categories WHERE category_id=$category_id");
+            if ($data2->num_rows > 0) {
+                $cat_data = $data2->fetch_assoc();
+                $file_viewable_to = $cat_data['viewable_to'];
+                // Converting $viewable_to to more readable text
+                if ($file_viewable_to == ',all,') {
+                    $file_view = "All";
+                } else {
+                    $file_viewable_to = substr($file_viewable_to, 1, -1);
+                    $file_view_array = explode(",", $file_viewable_to);
+                    $file_view = "";
+                    foreach ($file_view_array as $value) {
+                        $data3 = $link->query("SELECT name FROM rtc55314.groups WHERE group_id=$value");
+                        if ($data3->num_rows > 0) {
+                            $file_group_data = $data3->fetch_assoc();
+                            $file_group_name = $file_group_data['name'];
+                            $file_view = $file_view . $file_group_name . ", ";
                         }
-                        echo "</td></tr>";
                     }
-                    elseif($uploaded_at2 >= $join_group_date2 && $uploaded_at2 <= $left_group_date2){
-                        echo "<tr><td> <a href='download.php?name=". $file_location ."'> " . $file_name . "</a></td><td>" . $uploaded_by . "</td><td>" . $uploaded_at . "</td><td>" . $file_view . "</td><td>";
-                        // Allow delete for the person who uploaded the file
-                        if($uploaded_by == $_SESSION['username']){
-                            echo "<form class='form-button' method=post action='includes/delete_file_to_entity.php'>";
-                            echo "<button type='submit' name='delete' value=$file_id onclick=\"return confirm('Are you sure you want to delete this file?')\">Delete</button>";
-                            echo "</form>";
-                        }
-                        echo "</td></tr>";
-                    }
+                    $file_view = substr($file_view, 0, -2);
                 }
             }
+
+            // Display all the files uploaded to entity for admin, instructor, TA or all
+            if ($file_view == 'All' || $_SESSION['role_id'] < 4) {
+                echo "<tr><td> <a href='download.php?name=" . $file_location . "'> " . $file_name . "</a></td><td>" . $uploaded_by . "</td><td>" . $uploaded_at . "</td><td>" . $file_view . "</td><td>";
+                // Check if user has delete access
+                if ($_SESSION['role_id'] < 3 || $uploaded_by == $_SESSION['username']) {
+                    echo "<form class='form-button' method=post action='includes/delete_file_to_entity.php'>";
+                    echo "<button type='submit' name='delete' value=$file_id onclick=\"return confirm('Are you sure you want to delete this file?')\">Delete</button>";
+                    echo "</form>";
+                }
+                echo "</td></tr>";
+            }
+            // Display the files uploaded to entity available for student
+            else {
+                // Display the files that were available to the student when they were part of a particular group
+                foreach ($file_view_array as $value) {
+                    $data4 = $link->query("SELECT join_group_date, left_group_date FROM group_users WHERE group_id=$value AND user_id=" . $_SESSION['id']);
+                    if ($data4->num_rows > 0) {
+                        $group_data = $data4->fetch_assoc();
+                        $join_group_date = $group_data['join_group_date'];
+                        $left_group_date = $group_data['left_group_date'];
+                        $join_group_date2 = strtotime($join_group_date);
+                        $left_group_date2 = strtotime($left_group_date);
+                        $uploaded_at2 = strtotime($uploaded_at);
+                        if (!($join_group_date == "") && ($left_group_date == "")) {
+                            echo "<tr><td> <a href='download.php?name=" . $file_location . "'> " . $file_name . "</a></td><td>" . $uploaded_by . "</td><td>" . $uploaded_at . "</td><td>" . $file_view . "</td><td>";
+                            // Allow delete for the person who uploaded the file
+                            if ($uploaded_by == $_SESSION['username']) {
+                                echo "<form class='form-button' method=post action='includes/delete_file_to_entity.php'>";
+                                echo "<button type='submit' name='delete' value=$file_id onclick=\"return confirm('Are you sure you want to delete this file?')\">Delete</button>";
+                                echo "</form>";
+                            }
+                            echo "</td></tr>";
+                        } elseif ($uploaded_at2 >= $join_group_date2 && $uploaded_at2 <= $left_group_date2) {
+                            echo "<tr><td> <a href='download.php?name=" . $file_location . "'> " . $file_name . "</a></td><td>" . $uploaded_by . "</td><td>" . $uploaded_at . "</td><td>" . $file_view . "</td><td>";
+                            // Allow delete for the person who uploaded the file
+                            if ($uploaded_by == $_SESSION['username']) {
+                                echo "<form class='form-button' method=post action='includes/delete_file_to_entity.php'>";
+                                echo "<button type='submit' name='delete' value=$file_id onclick=\"return confirm('Are you sure you want to delete this file?')\">Delete</button>";
+                                echo "</form>";
+                            }
+                            echo "</td></tr>";
+                        }
+                    }
+                }
+            }
         }
-        
+        echo "</tbody></table>";
     }
-}
     ?>
