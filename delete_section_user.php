cf6ff7ce06fa642a60907c808d94d5a9b959a514
bajajp1994 <bajajp1994@gmail.com>
2022-04-18 22:13:30 -0700

    Added delete_section_user.php

---
+++ delete_section_user.php
@@ -0,0 +1,28 @@
+<?php
+//40197292
+/* Database credentials. */
+include "includes/head.php";
+
+// Check connection
+if($link == false) {
+	die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+if(!isset($_GET['section_id']) && !isset($_GET['user_id'] )){
+    // redirect to show page
+    die('id not provided');
+}
+
+$section_id= (int)$_GET['section_id'];
+$user_id=(int)$_GET['user_id'];
+
+$sql = "DELETE FROM users_sections WHERE section_id = '$section_id' and user_id='$user_id'";
+if ($link->query($sql) === TRUE) {
+             
+             echo "Section User deleted successfully!!";
+              } else {
+                 echo "Error deleting record: " . $link->error;
+            }
+// Close connection
+mysqli_close($link);
+
+?>
