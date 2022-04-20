dd40a7ade50cfa0cf82b0a95c87caf9ac5b106f4
Vikram Nisarga <vikramnisarga1@gmail.com>
2022-04-20 00:08:08 -0400

    section redirect

--- create_section_ta.php
+++ create_section_ta.php
@@ -3,74 +3,83 @@
 /* Database credentials. */
 include "includes/head.php";
 
-$id=0;
-$section_id=0;
- if(isset($_POST['submit']))
-        {  
-	$id =$_POST["id"];
-	$section_id=$id;
-		}
-		else{
-			$id = (int)$_GET['id'];
-			$section_id=$id;
-		}
+$id = 0;
+$section_id = 0;
+if (isset($_POST['submit'])) {
+    $id = $_POST["id"];
+    $section_id = $id;
+} else {
+    $id = (int)$_GET['id'];
+    $section_id = $id;
+}
 //$sql = mysqli_query($link,"SELECT section_name FROM sections WHERE section_id = '$id'");
 //$row2 = mysqli_fetch_array($sql);
 $query = "SELECT u.user_id,u.username FROM users u";
 $result = mysqli_query($link, $query);
 $options = "";
-while($row = mysqli_fetch_array($result)){
-    $options = $options."<option value='$row[0]'>$row[1]</option>";
+while ($row = mysqli_fetch_array($result)) {
+    $options = $options . "<option value='$row[0]'>$row[1]</option>";
 }
 
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-        
-            // Prepare an insert statement
-            $sql = "INSERT INTO users_roles_sections(user_id, section_id, role_id) VALUES (?, ?,?)";
-             if($sql == false) {
-	           die("ERROR: Could not connect. " . mysqli_error($link));
-                  }
-			 
-            if($stmt = mysqli_prepare($link, $sql)){
-                // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "iii",$param_ta_id,$param_section_id,$param_role_id);
-                
-                // Set parameters
-                $param_ta_id = $_POST["user_id"];
-				$param_section_id = $section_id;
-                $param_role_id = 3;
-                
-                // Attempt to execute the prepared statement
-                if(mysqli_stmt_execute($stmt)){
-                    // Redirect to previous page
-					header("location:manage_section_tas.php?id=$id");
-                   
-                } else{
-                    die('Error with execute: ' . htmlspecialchars($stmt->error));
-                }
-                // Close statement
-                mysqli_stmt_close($stmt);
-            }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Prepare an insert statement
+    $sql = "INSERT INTO users_roles_sections(user_id, section_id, role_id) VALUES (?, ?,?)";
+    if ($sql == false) {
+        die("ERROR: Could not connect. " . mysqli_error($link));
+    }
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "iii", $param_ta_id, $param_section_id, $param_role_id);
+
+        // Set parameters
+        $param_ta_id = $_POST["user_id"];
+        $param_section_id = $section_id;
+        $param_role_id = 3;
+
+        // Attempt to execute the prepared statement
+        if (mysqli_stmt_execute($stmt)) {
+            // Redirect to previous page
+            header("location:manage_section_tas.php?id=$id");
+        } else {
+            die('Error with execute: ' . htmlspecialchars($stmt->error));
+        }
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
 }
 
 ?>
 
-       <div class="content">
-        <h1>Associate TA</h1>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])."?id=".$id; ?>" method="post">
-			<div class="form-group">
-			   <label>TAs</label>
-			    <select name="user_id" id="user_id" class="form-control">
-                 <?php echo $options;?>
-                 </select>
-		     </div>
-		</br>
-	        <input type="hidden" name="id" value="<?php echo $id; ?>" />
-            <div class="form-group">
-                <input type="submit" name="submit" style='background-color:pink' value="Submit">
-            </div>
-        </form>
-     </div>
-    </div>
+<div class="content">
+
+    <?php
+    // Display success/error message
+    if (isset($_SESSION['message'])) {
+        echo "<font color='blue'>" . $_SESSION['message'] . "</font>";
+        unset($_SESSION['message']);
+    }
+    if (isset($_SESSION['error'])) {
+        echo "<font color='red'>" . $_SESSION['error'] . "</font>";
+        unset($_SESSION['error']);
+    }
+    ?>
+    <h1>Associate TA</h1>
+    <form action="<?php echo "includes/do_create_section_ta.php?id=" . $id; ?>" method="post">
+        <div class="form-group">
+            <label>TAs</label>
+            <select name="user_id" id="user_id" class="form-control">
+                <?php echo $options; ?>
+            </select>
+        </div>
+        </br>
+        <input type="hidden" name="id" value="<?php echo $id; ?>" />
+        <div class="form-group">
+            <input type="submit" name="submit" style='background-color:pink' value="Submit">
+        </div>
+    </form>
+</div>
+</div>
 </div>
