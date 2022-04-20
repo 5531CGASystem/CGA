c78a4fcb0b80d5faba2b094ad44caf6785a45c9c
Vikram Nisarga <vikramnisarga1@gmail.com>
2022-04-19 20:34:18 -0400

    fix edit section

--- edit_section.php
+++ edit_section.php
@@ -21,21 +21,22 @@ if ($result->num_rows != 1) {
 $data = $result->fetch_assoc();
 // Define variables and initialize with empty values
 $section_error = "";
-$query = "SELECT r.user_id,u.username FROM user_roles as r 
-join users as u
-on r.user_id=u.user_id 
-and r.role_id=2;";
+$query = "SELECT u.user_id,u.username FROM users u";
 $result2 = mysqli_query($link, $query);
 $options = "";
 while ($row2 = mysqli_fetch_array($result2)) {
-    $options = $options . "<option value='$row2[0]'>$row2[1]</option>";
+    $selected="";
+    if ($data['prof_id'] == $row2[0]){
+        $selected="selected";
+    }
+    $options = $options . "<option $selected value='$row2[0]'>$row2[1]</option>";
 }
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Check if section_name is empty
-    if (empty(trim($_POST["section_name"]))) {
-        $section_error = "Section name cannot be empty.";
+    /* if (empty(trim($_POST["section_name"]))) {
+        //$section_error = "Section name cannot be empty.";
     } else {
         // Prepare a select statement
         $sql = "SELECT section_id FROM sections WHERE section_name = ?";
@@ -49,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
-                /* store result */
+                // store result 
                 mysqli_stmt_store_result($stmt);
 
                 if (mysqli_stmt_num_rows($stmt) == 1) {
@@ -63,18 +64,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             // Close statement
             mysqli_stmt_close($stmt);
-        }
-    }
+        } 
+    }*/
     if (empty($section_error)) {
         // Prepare an insert statement
-        $sql = "UPDATE sections SET section_name=?, prof_id=? WHERE section_id=$id";
+        $sql = "UPDATE sections SET prof_id=? WHERE section_id=$id";
         if ($sql == false) {
-            die("ERROR: Could not connect. " . mysqli_error());
+            die("ERROR: Could not connect. " . mysqli_error($link));
         }
 
         if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "si", $param_section_name, $param_prof_id);
+            mysqli_stmt_bind_param($stmt, "i", $param_prof_id);
 
             // Set parameters
             $param_section_name = trim($_POST["section_name"]);
@@ -119,13 +120,10 @@ if (!empty($section_error)) {
     <h1>Edit Section</h1>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . '?id=' . $id); ?>" method="post">
         <div class="form-group">
-            <label>Section Name<font color='red'> *</font></label>
-            <input type="text" name="section_name" class="form-control <?php echo (!empty($section_error)) ? 'is-invalid' : ''; ?>" value="<?= $data['section_name'] ?>">
-            <span style='display: block;'><?php echo $section_error; ?></span>
+            <h2>Section Name: <?php echo $data['section_name']; ?></h2>
         </div>
-        </br>
         <div class="form-group">
-            <label>Instructor</label>
+            <label>Instructor:</label>
             <select name="prof_id" id="prof_id" class="form-control">
                 <?php echo $options; ?>
             </select>
